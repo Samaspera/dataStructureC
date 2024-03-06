2eb73ff8aa9f2713fe893d0b7d7a43446aa063f8
aspera <123@stu.edu.cn>
2024-03-06 23:07:38 +0800

    Sequence list ver0.1

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include "./libs/SequenceList.h"
+
+void testSequenceList();
 
 int main() {
-    printf("Hello, World!\n");
+    testSequenceList();
     return 0;
+}
+
+void testSequenceList() {
+    int i, a[101];
+    for (i = 0; i < 101; i++) {
+        a[i] = i + 1;
+    }
+    SequenceList *list = createList(a, 101);
+    if(list->length < 101) {
+        printf("failed");
+        return;
+    }
+    ElemType e[list->length];
+    traverse(*list, e);
+    for (i = 0; i < list->length; i++) {
+        printf("%d ", e[i].data);
+    }
 }
